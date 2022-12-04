817ba55318c67d165cfddbed99e0476871022e7f
owen2345 <owenperedo@gmail.com>
2022-12-04 07:07:09 -0400

    test: add missing tests

---
+++ spec/controllers/reservations_controller_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+describe ReservationsController, type: :controller do
+  xdescribe '#new' do
+  end
+
+  xdescribe '#create' do
+  end
+
+  xdescribe '#free_slots' do
+  end
+
+  xdescribe '#destroy' do
+  end
+end
---
+++ spec/features/reservations_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+describe 'Reservations' do
+  xdescribe 'when creating a new reservation' do
+  end
+
+  xdescribe 'when filtering for free slots' do
+  end
+end
